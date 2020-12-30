25d1ec9d6e6c3b6f963d59a4cc9804cb492782c1
Hemant Kuruva <111hemant111@gmail.com>
2020-12-30 15:07:32 -0500

    assignment 1 completed

---
+++ src/components/DishDetailComponent.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class DishDetail extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+        }
+    }
+
+    renderDish(dish){
+        if (dish != null) {
+            return(
+                <div key={dish.id}>
+                    <Card>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle><h4>{dish.name}</h4></CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+            );
+        }
+        else {
+            return (
+                <div></div>
+            );
+        }
+    }
+
+    renderComments(comment){
+        if (comment != null) {
+            const date = new Date(comment.date);
+            let monthArray = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+            let formattedDate = "" + date.getDate();
+            if (formattedDate.length === 1)
+            {
+                formattedDate = "0" + formattedDate;
+            }
+                
+            return(
+                <div key={comment.id}>
+                        <CardTitle>{comment.comment}</CardTitle>
+                        <CardTitle>{"-- " + comment.author + ", " + monthArray[date.getMonth()] + " " + formattedDate + " " + date.getFullYear()}</CardTitle>
+                </div>
+            );
+        }
+        else {
+            return (
+                <div></div>
+            );
+        }
+    }
+
+    render(){
+
+        let selectedDish = this.props.dishSelectionProp;
+
+        if (selectedDish != null){
+            return(
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-12 col-md-5 m-1">
+                                {this.renderDish(selectedDish)}
+                            </div>
+                            <div className="col-12 col-md-5 m-1">
+                                <CardTitle><h3>Comments</h3></CardTitle>
+                                {selectedDish.comments.map((comment) => {return (this.renderComments(comment));})}
+                            </div>
+                        </div>
+                    </div>
+                );
+        }
+
+        else
+        {
+            return(
+                <div></div>
+            );
+        }
+            
+        
+    }
+}
+
+export default DishDetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import DishDetail from './DishDetailComponent';
 
 class Menu extends Component {
     constructor(props) {
@@ -14,23 +15,10 @@ class Menu extends Component {
         this.setState({selectedDish: dish});
     }
 
-    renderDish(dish){
-        if (dish != null) {
-            return(
-                <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
-            );
-        }
-        else {
-            return (
-                <div></div>
-            );
-        }
+    getSelecBoxComp() {
+        return (
+            <DishDetail dishSelectionProp={this.state.selectedDish}></DishDetail>
+        )
     }
 
     render() {
@@ -46,7 +34,8 @@ class Menu extends Component {
               </div>
             );
         });
-        console.log(menu);
+
+        const selectionBox = this.getSelecBoxComp();
 
         return (
             <div className="container">
@@ -54,7 +43,7 @@ class Menu extends Component {
                     {menu}
                 </div>
                 <div className="row">
-                    {this.renderDish(this.state.selectedDish)}
+                    {selectionBox}
                 </div>
             </div>
         );
